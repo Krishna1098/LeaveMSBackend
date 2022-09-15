e4bb3cbde2cdb9bae76daf8da29d357596be26b1
Krishna1098 <61451165+Krishna1098@users.noreply.github.com>
2022-09-15 16:08:54 +0530

    Update Employee.cs

--- LeaveMS/Models/Employee.cs
+++ LeaveMS/Models/Employee.cs
@@ -16,7 +16,7 @@ namespace LeaveMS.Models
         public string EmpName { get; set; }
         public string Email { get; set; }
         public int LeavesInHand { get; set; }
-        public bool? Leavestatus { get; set; }
+        //public bool? Leavestatus { get; set; }
         public string Department { get; set; }
         public string Designation { get; set; }
         public string Location { get; set; }
